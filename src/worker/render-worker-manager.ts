ff9969f4c9e28da98fc3f59a9d65be4ade4607bc
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-01-09 23:38:08 +0100

    Prepare the render worker manager

---
+++ src/worker/render-worker-manager.ts
@@ -0,0 +1,32 @@
+/**
+ * Prepares listening to a response from a worker.
+ * @returns the id which will be listened to,
+ * which that should be passed to the worker so that it includes it in the response,
+ * and a promise that resolves to the response.
+ */
+function promiseOfResponse<T>(worker: Worker): [string, Promise<T>] {
+    // To avoid mixups between different render calls, we use a unique ID
+    const id = crypto.randomUUID();
+
+    // Prepare reception of the response
+    const prom = new Promise<T>((resolve) => {
+        const listener = ({ data: { id: receivedId, result } }: MessageEvent) => {
+            if (receivedId === id) {
+                worker.removeEventListener("message", listener);
+                resolve(result);
+            }
+        };
+        worker.addEventListener("message", listener);
+    });
+
+    return [id, prom];
+}
+
+export interface RenderWorkManager {
+}
+interface RenderWorkManagerConstructor {
+    new(): RenderWorkManager;
+}
+export const newWorkerManager: RenderWorkManagerConstructor = typeof Worker !== "undefined" ?
+    class ActualWorkerManager {} :
+    class FallbackWorkManager {};
